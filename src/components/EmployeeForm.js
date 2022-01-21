6566095f3c91545195e25bc6603331e95a4e2d68
Casey MacLeod <caseymacleod22@gmail.com>
2022-01-21 14:21:59 -0500

    MVP finished

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -1,69 +1,67 @@
-import { useState } from "react";
+import { useState } from "react"
 
 export function EmployeeForm() {
-    const [employees, setEmployees] = useState([])
-    const [employeeId, setEmployeeId] = useState('')
-    const [update, setUpdate] = useState(false)
-    
+
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
+    const [update, setUpdate] = useState(false)
+    const [employees, setEmployees] = useState([])
+    const [employeeId, setEmployeeId] = useState([])
 
-    const createEmployee = async () => {
-        try {
-        const res = await fetch('/api/employees', 
-        {method: 'POST', body: JSON.stringify({firstName, lastName})})
-        const json = await res.json()
-
-        setEmployees([...employees, json.employee])
-        setFirstName('')
-        setLastName('')
-    } catch (error) {
-        console.log('Something went wrong')
-    }
-}
-const updateEmployee = async () => {
+    const submitForm = async (event) => {
+        event.preventDefault()
+      
+        if(update){
+            updateEmployee()
+        }
+      }
+      
+      const updateEmployee = async () => {
         try {
         const res = await fetch(`/api/employees/${employeeId}`, 
         {method: 'PATCH', body: JSON.stringify({firstName, lastName})})
         const json = await res.json()
-
+  
         const employeesCopy = [...employees]
         const index = employees.findIndex((employee) => employee.id === employeeId)
         employeesCopy[index] = json.employee
-
+  
         setEmployees(employeesCopy)
         setFirstName('')
         setLastName('')
         setUpdate(false)
-        setEmployeeId('')
+        setEmployeeId(null)
     } catch (err) {
         console.log(err)
     }
-}
+  }
 
-const submitForm = async (event) => {
-    event.preventDefault()
-
-    if(update){
-        updateEmployee()
-    } else {
-        createEmployee()
-    }
+  const setEmployeeToUpdate = (id) => {
+    const employee = employees.find(emp => emp.id === id)
+    if(!employee) return
+    setUpdate(true)
+    setEmployeeId(employee.id)
+    setFirstName(employee.firstName)
+    setLastName(employee.lastName)
+    console.log('working')
 }
 
     return (
         <form onSubmit={submitForm}>
-            <div>
-                <div>
-                    <input type="text" placeholder={} value={firstName} onChange={e => setFirstName(e.target.value)}/>
-                </div>
-                <div>
-                <input type="text" placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)} />
-                </div>
-                <div>
-                    <button type='submit'>{update ? 'Update' : 'Create'}</button>
-                </div>
-            </div>
-        </form>
+        <div>
+          <div>
+             <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)}/>
+          </div>
+          <div>
+            <input type="text" value={lastName} onChange={e => setLastName(e.target.value)}/>
+            {employees.map(e => (
+                <button onClick={() => setEmployeeToUpdate(e.id)}>Update</button>
+            ))}
+          </div>
+          <div>
+            <button type='submit' onClick={console.log('test')}>Submit</button>
+          </div>
+        </div>
+      </form>
     )
 }
--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,6 +1,7 @@
 import Modal from "react-modal/lib/components/Modal"
 import { useState, useEffect } from "react"
 import React from "react"
+import { EmployeeForm } from "./EmployeeForm";
 // import { EmployeeForm } from "./EmployeeForm"
 
 const customStyles = {
@@ -14,6 +15,16 @@ const customStyles = {
   },
 };
 
+  const styleUl = {
+    listStyleType: 'none'
+  }
+
+  const avatarStyling = {
+    height: '60px',
+    width: '100'
+
+  }
+
 export function EmployeeTable() {
     const [employees, setEmployees] = useState([])
     const [employeeId, setEmployeeId] = useState([])
@@ -22,31 +33,14 @@ export function EmployeeTable() {
 
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
-    const [info, setInfo] = useState('')
+    const [selectedEmployee, setSelectedEmployee] = useState('')
 
     useEffect(() => {
       fetch('/api/employees')
       .then(res => res.json())
       .then(json => setEmployees(json.employees)
       )
-      // console.log(expand)
     }, [])
-
-    let subtitle;
-    const [modalIsOpen, setIsOpen] = React.useState(false);
-  
-    function openModal() {
-      setIsOpen(true);
-    }
-  
-    function afterOpenModal() {
-      // references are now sync'd and can be accessed.
-      subtitle.style.color = '#f00';
-    }
-  
-    function closeModal() {
-      setIsOpen(false);
-    }
 
     const expandAllEmployees = () => {
       if(expand === false) {
@@ -54,36 +48,35 @@ export function EmployeeTable() {
       } if(expand === true) {
         setExpand(false)
       }
-      console.log(expand)
     }
 
-    const updateEmployee = async () => {
-      try {
-      const res = await fetch(`/api/employees/${employeeId}`, 
-      {method: 'PATCH', body: JSON.stringify({firstName, lastName})})
-      const json = await res.json()
+//     const updateEmployee = async () => {
+//       try {
+//       const res = await fetch(`/api/employees/${employeeId}`, 
+//       {method: 'PATCH', body: JSON.stringify({firstName, lastName})})
+//       const json = await res.json()
 
-      const employeesCopy = [...employees]
-      const index = employees.findIndex((employee) => employee.id === employeeId)
-      employeesCopy[index] = json.employee
+//       const employeesCopy = [...employees]
+//       const index = employees.findIndex((employee) => employee.id === employeeId)
+//       employeesCopy[index] = json.employee
 
-      setEmployees(employeesCopy)
-      setFirstName('')
-      setLastName('')
-      setUpdate(false)
-      setEmployeeId(null)
-  } catch (err) {
-      console.log(err)
-  }
-}
+//       setEmployees(employeesCopy)
+//       setFirstName('')
+//       setLastName('')
+//       setUpdate(false)
+//       setEmployeeId(null)
+//   } catch (err) {
+//       console.log(err)
+//   }
+// }
 
-const submitForm = async (event) => {
-  event.preventDefault()
+// const submitForm = async (event) => {
+//   event.preventDefault()
 
-  if(update){
-      updateEmployee()
-  }
-}
+//   if(update){
+//       updateEmployee()
+//   }
+// }
     
   
 const deleteEmployee = async (id) => {
@@ -103,97 +96,50 @@ const deleteEmployee = async (id) => {
         setLastName(employee.lastName)
     }
 
-    const moreInfo = async(id) => {
-      try {
-        openModal(true)
-        const res = await fetch(`/api/employees/${id}`)
-        const json = await res.json
-         setInfo(json.info) 
 
-      } catch (error) {
-        
-      }
-    }
+return (
+  <div>
+    <div>
+      <>
+      <EmployeeForm />
+      <button onClick={() => expandAllEmployees()}>{expand ? 'COLLAPSE ALL' : 'EXPAND ALL'}</button>
+              <ul style={styleUl}>
+                {employees.map(e => (
+                <li>
+                <img src={e.avatar} alt="avatar" style={avatarStyling} key={e.id} onClick={() => setSelectedEmployee(e)}/>
+                <li>{e.firstName} {e.lastName}</li>
+                  <button onClick={() => setEmployeeToUpdate(e.id)}>Update</button>
+                  <button onClick={() => deleteEmployee(e.id)}>Delete</button>        
+                  {expand === true ? (
 
-    return (
-        <div>
-        <div>
-        </div>
-        <div>
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>More Info</h2>
-        <button onClick={closeModal}>close</button>
-        {employees.slice(0, 1).map(info => (
-          <>
-          <p>{info.firstName}</p>
-          <p>{info.lastName}</p>
-          <p>{info.email}</p>
-          <p>{info.phone}</p>
-          <p>{info.address.streetAddress}, {info.address.city} {info.address.state} {info.address.zipCode}</p>
-          <p>{info.bio}</p>
-          </>
-        ))}
-      </Modal>
-    </div>
-            <table>
-              <tbody>
-              <tr>
-                <td>
-                  <button onClick={() => expandAllEmployees()}>{expand ? 'COLLAPSE ALL' : 'EXPAND ALL'}
-                </button>
-                </td>
-              </tr>
-                {employees.filter(item => item).map(({id, avatar, firstName, lastName, email, phone, bio, address}) => {
-                  return(
-                    <tr key={id}>
-                    <td><img src={avatar} alt="avatar" style={{height: '60px', width:'100px'}}/></td>
-                    <td>{firstName}</td>
-                    <td>{lastName}</td>
-                    {expand === true ? (
-                      <table>
-                        <thead>
-                          <tr>
-                          <td>{email}</td>
-                          <td>{phone}</td>
-                          <td>{address.streetAddress}</td>
-                          <td>{address.city}, {address.state} {address.zipCode}</td>
-                          <td>{bio}</td>  
-                          {console.log(email)}                  
-                          </tr>
-                        </thead>
-                      </table>
+                        <ul style={styleUl}>
+                        <li>{e.email}</li>
+                        <li>{e.phone}</li>
+                        <li>{e.address.streetAddress}</li>
+                        <li>{e.address.city}, {e.address.state} {e.address.zipCode}</li>
+                        <li>{e.bio}</li>  
+                        </ul>
+
                       ) : (
-                      <th></th>
+                      <li></li>
                     )}
-                    <td>
-                        <button onClick={() => setEmployeeToUpdate(id)}>Update</button>
-                        <button onClick={() => moreInfo(id)}>More Info</button>
-                        <button onClick={() => deleteEmployee(id)}>Delete</button>        
-                    </td>
-                  </tr>
-                  )
-                })}
-              </tbody>
-            </table>
-                <form onSubmit={submitForm}>
-            <div>
-                <div>
-                    <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)}/>
-                </div>
-                <div>
-                <input type="text" value={lastName} onChange={e => setLastName(e.target.value)}/>
-                </div>
-                <div>
-                    <button type='submit'>{update ? 'Update' : ''}</button>
-                </div>
-            </div>
-        </form>
-      </div>
-      )
+              </li>
+            ))}
+          </ul>
+          </>
+          <Modal isOpen={selectedEmployee}
+                 style={customStyles}
+          >
+          <img src={selectedEmployee?.avatar} alt='avatar' style={avatarStyling} />
+          <p>{selectedEmployee?.firstName}</p>
+          <p>{selectedEmployee?.lastName}</p>
+          <p>{selectedEmployee?.email}</p>
+          <p>{selectedEmployee?.phone}</p>
+          <p>{selectedEmployee?.address?.streetAddress} {selectedEmployee?.address?.city}, {selectedEmployee?.address?.state} {selectedEmployee?.address?.zipCode}</p>
+          <p>{selectedEmployee?.bio}</p>
+          <button onClick={() => setSelectedEmployee(null)}>Close</button>
+        </Modal>
+    </div>
+  </div>
+    )
 }
